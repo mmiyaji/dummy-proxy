16b3268950cecf0264f1f56bdb9bc5a2e96fc270
mmiyaji <ruhenheim1223@gmail.com>
2024-02-11 22:16:33 +0900

    init

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const cors = require('cors');
+const path = require('path');
+const { format } = require('date-fns');
+const axios = require('axios');
+const headers = {
+    'Content-Type': 'application/json; charset=utf-8',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Access-Control-Allow-Origin': '*',
+}
+
+const app = express();
+const port = process.env.PORT || 3011;
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/proxy', async (req, res, next) => {
+  console.log(req.url, req.query);
+  try {
+    const queryUrl = req.query.url || "https://public.openrec.tv/external/api/v5/movies?sort=created_at&channel_ids=simoyaka,Yaritaiji,ringomoti";
+    if(queryUrl != ""){
+      const decodeUrl = decodeURIComponent(queryUrl);
+      // const encodedString = encodeURIComponent(originalString);
+      response = await axios.get(decodeUrl);
+      // console.log(response.data);  
+      res.json(response.data);
+    }else{
+      res.json([]);
+    }
+  
+  } catch (error) {
+    // next(error);
+    res.json([]);
+  }
+});
+
+app.get("/", (req, res) => res.type('html').send(html));
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      Hello from Render!
+    </section>
+  </body>
+</html>
+`
